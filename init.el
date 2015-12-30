34625720eb8733c15b674e5be8c40658eecd4f06
cmacrae <calum0macrae@gmail.com>
2015-12-30 20:50:41 +0000

    Initial commit

---
+++ init.el
@@ -0,0 +1,26 @@
+;;; init.el --- Where all the magic begins
+;;
+;; This file loads Org-mode and then loads the rest of the Emacs initialization from Emacs lisp
+;; embedded in the literate Org-mode file: emacs.org
+(package-initialize)
+
+(setq emacs-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+
+(let* ((org-dir (expand-file-name
+                 "lisp" (expand-file-name
+                         "org" (expand-file-name
+                                "src" emacs-dir))))
+       (org-contrib-dir (expand-file-name
+                         "lisp" (expand-file-name
+                                 "contrib" (expand-file-name
+                                            ".." org-dir))))
+       (load-path (append (list org-dir org-contrib-dir)
+                          (or load-path nil))))
+  ;; load up Org-mode and Org-babel
+  (require 'org-install)
+  (require 'ob-tangle))
+
+;; load up all literate org-mode files in this directory
+(mapc #'org-babel-load-file (directory-files emacs-dir t "\\.org$"))
+
+;;; init.el ends here
