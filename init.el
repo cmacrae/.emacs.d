00dc892f818436bb8488e75f0fcf10b2eadc0e10
cmacrae <calum0macrae@gmail.com>
2017-04-12 19:30:32 +0100

    Remove dependency on external org source (no longer needed)

--- init.el
+++ init.el
@@ -18,19 +18,9 @@
 
 (setq emacs-dir (file-name-directory (or (buffer-file-name) load-file-name)))
 
-(let* ((org-dir (expand-file-name
-                 "lisp" (expand-file-name
-                         "org" (expand-file-name
-                                "src" emacs-dir))))
-       (org-contrib-dir (expand-file-name
-                         "lisp" (expand-file-name
-                                 "contrib" (expand-file-name
-                                            ".." org-dir))))
-       (load-path (append (list org-dir org-contrib-dir)
-                          (or load-path nil))))
-  ;; load up Org-mode and Org-babel
-  (require 'org-install)
-  (require 'ob-tangle))
+;; load up Org-mode and Org-babel
+(require 'org-install)
+(require 'ob-tangle)
 
 ;; load up all literate org-mode files in this directory
 (mapc #'org-babel-load-file (directory-files emacs-dir t "\\.org$"))
