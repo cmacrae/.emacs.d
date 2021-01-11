f84ae578e4b09c79b7d34ceaf18055162e18af4b
Calum MacRae <hi@cmacr.ae>
2021-01-11 17:37:26 +0000

    refactor: Initial commit

--- init.el
+++ init.el
@@ -13,10 +13,24 @@
   (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t))
 (package-initialize)
 
-;; Bootstrap use-package
+;; Bootstrap use-package & straight.el
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+(straight-use-package 'use-package)
 
 (setq emacs-dir (file-name-directory (or (buffer-file-name) load-file-name)))
 
@@ -26,5 +40,4 @@
 
 ;; load up all literate org-mode files in this directory
 (mapc #'org-babel-load-file (directory-files emacs-dir t "\\.org$"))
-
 ;;; init.el ends here
